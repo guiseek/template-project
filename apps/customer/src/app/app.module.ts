27bea39bb81263c339b0f8b4ec017ea8f04aab8f
Gui Seek <guiseek@gmail.com>
2019-10-27 14:12:19 -0300

    lint tags, nx module

--- apps/customer/src/app/app.module.ts
+++ apps/customer/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NxModule } from '@nrwl/angular';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    NxModule.forRoot(),
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
   providers: [],
