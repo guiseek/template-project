19a2aa30d2bdb34cdd71dfd7118b53e3fde12e0f
Gui Seek <guiseek@gmail.com>
2019-10-28 02:35:08 -0300

    reorganize ui from lib to container

--- libs/ui/src/lib/ui.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavModule } from './ui/nav/nav.module';
-
-@NgModule({
-  imports: [CommonModule, NavModule]
-})
-export class UiModule {}
