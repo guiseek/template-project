e58630856ee5020babf9086df92d97f8ba0f39c5
Gui Seek <guiseek@gmail.com>
2019-10-29 18:09:04 -0300

    base material

--- apps/webapp/src/app/app.component.ts
+++ apps/webapp/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webapp';
+
+  calling = false;
+
+
+  ngOnInit() {
+    window.setInterval(() => {
+      this.calling = !this.calling;
+    }, 3000)
+  }
 }
